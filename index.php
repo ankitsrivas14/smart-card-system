c1554cc047bf84d1eb89d8da8df9ed8cebd97b54
Ankit Srivastava <ankitsrivas14@gmail.com>
2020-11-06 20:07:00 +0530

    First version

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Font Awesome CSS -->
+    <link href="css/all.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>SMART CARD SYSTEM | Integrated with RFID and IoT</title>
+  </head>
+  <body>
+        <div class="container-fluid header">
+            <span>SMART CARD SYSTEM - Integrated with RFID and IoT</span>
+        </div>
+
+        <div class="container mainBody">
+            <div class="row ribbon">
+                <span><i class="fas fa-user"></i> User Identification</span>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md box" id="#">
+                            <a href="progress.php"><img src="images/aadhar.jpg"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/pancard.jpg"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/eci.jpg"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr style="border-width:10px;">
+
+            <div class="row ribbon">
+                <span><i class="fas fa-car-alt"></i> Transport Sector</span>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/dl.png"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/rc.jpeg"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/carinsurance.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/dmrc.jpg"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/dtc.png"></a>
+                        </div>
+                        <div class="col-md">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr style="border-width:10px;">
+
+            <div class="row ribbon">
+                <span><i class="fas fa-heartbeat"></i> Heathcare Sector</span>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/healthinsurance.jpeg"></a>
+                        </div>
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/cghs.jpg"></a>
+                        </div>
+                        <div class="col-md">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr style="border-width:10px;">
+
+            <div class="row ribbon">
+                <span>Ration System</span>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md box">
+                            <a href="progress.php"><img src="images/ration.png"></a>
+                        </div>
+                        <div class="col-md">
+                        </div>
+                        <div class="col-md">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  </body>
+</html>
